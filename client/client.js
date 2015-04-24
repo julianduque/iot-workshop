03c70f8b6ff0f7c286964223714ebd4dc5fabef5
Julian Duque <julianduquej@gmail.com>
2015-04-24 10:14:15 -0500

    Add an RGB Led to the board and control it with MQTT

--- client/client.js
+++ client/client.js
@@ -7,8 +7,21 @@ board.on('ready', function () {
   var client = mqtt.connect()
   client.subscribe('colors')
 
+  var led = new five.Led.RGB({
+    pins: {
+      red: 'A6',
+      green: 'A9',
+      blue: 'A10'
+    }
+  })
+
   client.on('message', function (topic, payload) {
     console.log('Received ' + payload + ' sent to ' + topic)
+
+    if (topic === 'colors') {
+      var color = payload.toString()
+      led.color(color)
+    }
   })
 
 })
