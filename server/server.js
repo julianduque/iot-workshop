0ffb108353f84a44569ca59a094d4a38910de2df
Julian Duque <julianduquej@gmail.com>
2015-04-24 09:10:00 -0500

    Add WebSockets support using socket.io

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
-var http    = require('http')
-var path    = require('path')
-var express = require('express')
+var http     = require('http')
+var path     = require('path')
+var express  = require('express')
+var socketio = require('socket.io')
 
 var port = process.env.PORT || 8080
 
@@ -10,6 +11,13 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 // setup http server
 var server = http.createServer(app)
+
+// setup websockets server
+var io = socketio(server)
+
+io.on('connection', function (socket) {
+  console.log('Client connected ' + socket.id)
+})
 
 server.listen(port, function () {
   console.log('Server listening on port ' + port)
